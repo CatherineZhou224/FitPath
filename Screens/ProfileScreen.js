d9c60f1e36df37136a64b22235a871b83e4d202f
CatherineZhou224 <cathzhou@umich.edu>
2024-12-04 12:51:08 -0800

    profile overlay

--- Screens/ProfileScreen.js
+++ Screens/ProfileScreen.js
@@ -1,24 +1,82 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import { getAuthUser } from "../AuthManager";
-import { Icon } from "@rneui/themed";
-import { Button } from "@rneui/themed";
+import { Button, Overlay, Icon, Input } from "@rneui/themed";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 function ProfileScreen({ navigation }) {
+  const [overlayVisible, setOverlayVisible] = useState(false);
+  const [inputText, setInputText] = useState(0);
+
+  const dispatch = useDispatch();
+  //const goal = useSelector((state) => state.goal);
+
+  //   useEffect(() => {
+  //     dispatch(fetchGaol());
+  //   }, []);
+
   return (
     <View style={styles.container}>
-      <View style={styles.welcome}>
-        <Icon
-          style={styles.icon}
-          name="hand-wave"
-          type="material-community"
-          size={20}
-          color="#000"
-        />
-        <Text style={styles.text}>Hi, {getAuthUser().displayName}!</Text>
+      <View>
+        <View style={styles.welcome}>
+          <Icon
+            style={styles.icon}
+            name="hand-wave"
+            type="material-community"
+            size={20}
+            color="#000"
+          />
+          <Text style={styles.text}>Hi, {getAuthUser().displayName}!</Text>
+        </View>
+        <TouchableOpacity>
+          <Button
+            onPress={() => {
+              setOverlayVisible(true);
+            }}
+          >
+            <Text>Add Goal</Text>
+          </Button>
+        </TouchableOpacity>
       </View>
-      <Button>
-        <Text>Add Goal</Text>
-      </Button>
+      <Overlay
+        isVisible={overlayVisible}
+        onBackdropPress={() => setOverlayVisible(false)}
+        overlayStyle={styles.overlayView}
+      >
+        <View style={styles.inputBox}>
+          <Text style={[styles.text, { color: "grey" }]}>{inputText}</Text>
+          <Text style={styles.text}>hrs/week</Text>
+        </View>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-around",
+            width: "80%",
+          }}
+        >
+          <Button
+            title="Cancel"
+            onPress={() => {
+              setInputText("");
+              setOverlayVisible(false);
+            }}
+          />
+          <Button
+            title="Save"
+            onPress={() => {
+              //   if (selectedItem) {
+              //     dispatch(
+              //       updateGroup({ group: selectedItem, inputName: inputText })
+              //     );
+              //   } else {
+              //     dispatch(addGroup(inputText));
+              //   }
+              setInputText("");
+              setOverlayVisible(false);
+            }}
+          />
+        </View>
+      </Overlay>
     </View>
   );
 }
@@ -34,11 +92,24 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  inputBox: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 10,
+    marginBottom: 20,
+  },
   icon: {
     marginRight: 5,
   },
   text: {
     fontSize: 18,
+  },
+  overlayView: {
+    flex: 0.2,
+    justifyContent: "center",
+    alignItems: "center",
+    width: "80%",
   },
 });
 
